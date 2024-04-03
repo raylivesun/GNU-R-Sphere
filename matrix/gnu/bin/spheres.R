d18270ce66a77396da8ac33689dcf251ad6c5941
raylivesun <162396999+raylivesun@users.noreply.github.com>
2024-04-03 17:14:05 -0300

    GNU-R-Sphere
    Auto make

---
+++ matrix/gnu/bin/spheres.R
@@ -0,0 +1,753 @@
+#!/usr/bin/r
+
+# A consists of the usual
+# Leibniz inequality together with the inequality
+La <- function(a = -1) {
+  if (a || is.array(a)) {
+      a <- rank(a, na.last = TRUE, ties.method = c("average", "first", "last",
+                                                   "random", "max", "min"))
+      print(a)
+  } else {
+    return(a)
+  }
+}
+Lb <- function(b = -1) {
+  if (b || is.array(b)) {
+      b <- rank(b, na.last = TRUE, ties.method = c("average", "first", "last",
+                                                 "random", "max", "min"))
+    print(b)
+  } else {
+    return(b)
+  }
+}
+L0 <- function(a = -1) {
+  if (a || is.array(a)) {
+      a <- rank(a, na.last = TRUE, ties.method = c("average", "first", "last",
+                                                 "random", "max", "min"))
+    print(a)
+  } else {
+    return(a)
+  }
+}
+
+# needs about the notion inquire
+G = 7
+H = 8
+# notion inquire division
+A = c(G/H)
+# units values inquire
+U = 4567
+N = 1
+Z = 9745
+N = Z>0
+# units girl value once
+Un = 8567
+Hn = 8567
+G = c(Un,Hn)
+# units cheers check files
+Bn = c(Hn)
+Nw = G;Bn;Un 
+L = G;A
+# portability expression of values
+Lbn = 2121
+G = Bn;La(a = -1);A;Lbn;Bn;c(A,La(a = -1));c(Bn, Lbn) 
+# check partial files
+E = 5
+E > 0
+# values types one unit
+N = 1
+n = 2
+n > N
+# check of analysis life cycles and start lives
+Ln = c(A,Bn);c(A,Bn)
+Ln;c(A,Bn);La(a = -1);Lbn;E
+Zn = 0+0+Inf
+# smile reference values get units values
+A <- c(c(Zn,Ln,G,Lbn,Bn,Nw,U,Z), type = c("O", "I", "F", "M", "2"))
+# 1) L is Leibniz if it satisfies the inequality
+Lab <- function(La, a, Lb, b){
+    La(a = -1)
+    Lb(b = -1)
+    if(a != is.array(a)) {
+       a <- rank(a, na.last = TRUE, ties.method = c("average", "first", "last",
+                                                    "random", "max", "min"))
+       print(a)
+    } else {
+      return(a)
+    }
+    if (b != is.array(b)) {
+        b <- rank(b, na.last = TRUE, ties.method = c("average", "first", "last",
+                                                     "random", "max", "min"))
+        print(b)
+    } else {
+      return(b)
+    }
+}
+# discursiveness in values in units
+a = 1
+b = 2
+# chap sweet values
+a;b = A
+
+# 2) L is strongly Leibniz if it is Leibniz, and L(1) = 0, and if for
+# any a ∈ A that has an inverse in A, we have
+L = c(1)
+L = 0
+# natural inverse link Leibniz
+a;A = inverse.gaussian(link = "1/mu^2");La(a = -1)<a-1+2+La(a = -1)
+# 3) L is finite if L(a) < ∞ for all a ∈ A.
+L = Inf;La(a = -1)<Inf+for(a in A) {
+  print(a)
+}
+# 4) L is semi finite if {a : L(a) < ∞} is norm-dense in A.
+L = a;La(a = -1)<Inf
+# 5) L is continuous if it is norm-continuous.
+L = normalizePath(path = "./", winslash = "\\", mustWork = NA)
+# 6) L is lower semi continuous if for one r ∈ R>0 , hence for all r > 0,
+# the set
+R = 16
+r = R>0
+r>0
+# check reference map handles units values get or put
+a;A = La(a = -1)<r
+L;a = La(a = -1)
+L;L = La(a = -1);a;A
+L;L = 0
+# satisfy values to handle units get once
+L1 = 211
+L2 = 212
+# satisfy value get once handle magic 
+A;r;R = L+1-6;r;L;L1;L2;L1+L2
+Va = 281
+# static values get once units
+La(a = -1);A;L = Va;La(a = -1)
+La(a = -1);A;L = subset.default(-1, subset = FALSE)
+L;L;A;La(a = -1);L = subset.default(-1, subset = FALSE)
+A;L+1-6;L = subset.default(-1, subset = FALSE)
+# cos reference good view to new values
+L;L = La(a = -1)
+A;L+1-6;L = subset.default(-1, subset = FALSE)
+La(a = -1);A;B = A;B;L0(a = -1);B;L;A;L0(a = -1);a;B;Inf
+La(a = -1);A;B = A;B+Inf;A;B;L
+L0 <- function(f = -1) {
+  if (f || is.array(f)) {
+      f <- rank(f, na.last = TRUE, ties.method = c("average", "first", "last",
+                                                 "random", "max", "min"))
+    print(f)
+  } else {
+    return(f)
+  }
+}
+# check founds type local units values
+f = 2
+A = c(0,1);B = L0(f = -1);f
+# analysis deny message units view child view
+Af = a;La(a = -1)<Inf
+# check analysis handle text values util view
+Af;A;L;a;Af;Af;a;Af;Af;A;1;Af = a;La(a = -1)<Inf
+# Proposition 1.3. If L is strongly Leibniz then AF is a spectral stable
+# sub algebra of A.
+Af;L;A = a;La(a = -1)<Inf
+# proposition 1.4 window static open and close to handle get once
+A;L;A;B;A;A = if(L || length(0L)) {
+  print(B)
+}
+# proposition 1.5 check pass analysis cake overflow
+A;L;A = L;L;c(L);L;L;L
+# output check list FM memory
+Cm <- function() {
+  return(b)
+}
+Fm <- function(b) {
+  Cm();
+    return (b);
+    print(a)
+}
+# check arguments about static of reference
+IZx <- function(ln = -2) {
+   return(ln*2)
+}
+wnd.setTimeout <- function(d, c) {
+  returnValue(default = NULL)
+}
+d <- function() {
+  a = IZx(ln = -2);c(2);
+  print(a)
+}
+m <- function() {
+  return(d())
+}
+on <- function(b, c) {
+  m();
+  d <- function() {
+    a = IZx(-b);c(c);
+    a || wnd.setTimeout(d, c)
+    print(a)
+  }
+  wnd.setTimeout(d, c)
+  d()
+}
+
+mId <- function(){
+  start(A,B)
+}
+Em <- function() {
+  returnValue(default = NULL)
+}
+KHD <- function(a) {
+  return(a^2)
+}
+N <- function(a, b = 101) {
+  returnValue(default = NULL)
+}
+Nm <- function(f, t) {
+  returnValue(default = NULL)
+} 
+LHD <- function(a) {
+  return(a)
+}
+Hm <- function(e) {
+  return(e)
+}
+Gm <- function(b, c, d) {
+  e = f;
+  e = Em();
+    if (b) {
+        a = KHD(a);
+        if (length(0L)) {
+          f = a;
+          Nm(f, t);
+        } else {
+          LHD(a)
+      }
+    } else {
+      return(b, c, d)
+      N(a, b = 101)
+    }
+    Hm(e)
+}
+
+i <- function(e, r) {
+   if (e != is.array(e)) {
+       e <- sum(e^r)
+       print(e)
+   } else {
+     return(e^r)
+   }
+}
+u <- function(e, t) {
+  i(e, r)
+    r = 0 && c(r = !0);
+    i = 1 && t;
+    if (is.array(e) || is.double(t)) {
+        e <- prod(e^t)
+        print(e)
+    } else {
+      return(e)
+    }
+   n = sin(e)
+   print(n^e)
+}
+Fl <- function() {
+   return(FALSE)
+}
+
+Em <- function() {
+ a = sin(5^27);
+ Am = cos(a)
+ e0x = expm1(Am)
+ xm = 1
+  if (a != 0) {
+    a = Fl();
+    if (a - Am > e0x) {
+      Am = a;
+      Bm = wnd.setTimeout(d = 4, c = 10)
+    }
+  }
+  if (xm !=  0) {
+       return(0)
+      }
+  return(FALSE)
+}
+
+m <- function(a) {
+  b = c(0,1)
+  a.b = abs(b)
+  if (a > 1) {
+      print(a*b^a.b)
+  } else {
+    return(a)
+  }
+}
+
+Dm <- function (from = 1, to = 1, by = ((to - from)/(length.out -                                                        1)), length.out = NULL, along.with = NULL, ...) 
+{
+  is.logint <- function(.) (is.integer(.) || is.logical(.)) && 
+    !is.object(.)
+  if ((One <- nargs() == 1L) && !missing(from)) {
+    lf <- length(from)
+    return(if (mode(from) == "numeric" && lf == 1L) {
+      if (!is.finite(from)) stop("'from' must be a finite number")
+      1L:from
+    } else if (lf) 1L:lf else integer())
+  }
+  if (!missing(along.with)) {
+    length.out <- length(along.with)
+    if (One) 
+      return(if (length.out) seq_len(length.out) else integer())
+    intn1 <- is.integer(length.out)
+  }
+  else if (!missing(length.out)) {
+    len <- length(length.out)
+    if (!len) 
+      stop("argument 'length.out' must be of length 1")
+    if (len > 1L) {
+      warning("first element used of 'length.out' argument")
+      length.out <- length.out[1L]
+    }
+    if (!(intn1 <- is.logint(length.out))) 
+      length.out <- as.numeric(ceiling(length.out))
+  }
+  chkDots(...)
+  if (!missing(from) && length(from) != 1L) 
+    stop("'from' must be of length 1")
+  if (!missing(to) && length(to) != 1L) 
+    stop("'to' must be of length 1")
+  if (!missing(from) && !is.finite(if (is.character(from)) from <- as.numeric(from) else from)) 
+    stop("'from' must be a finite number")
+  if (!missing(to) && !is.finite(if (is.character(to)) to <- as.numeric(to) else to)) 
+    stop("'to' must be a finite number")
+  if (is.null(length.out)) 
+    if (missing(by)) 
+      from:to
+  else {
+    int <- is.logint(from) && is.logint(to)
+    del <- to - if (int) 
+      as.double(from)
+    else from
+    if (del == 0 && to == 0) 
+      return(to)
+    if (length(by) != 1L) 
+      stop("'by' must be of length 1")
+    if (!is.logint(by)) 
+      int <- FALSE
+    else if (!int) 
+      storage.mode(by) <- "double"
+    n <- if (finite.del <- is.finite(del)) 
+      del/by
+    else to/by - from/by
+    if (!is.finite(n)) {
+      if (!is.na(by) && by == 0 && del == 0) 
+        return(from)
+      stop("invalid '(to - from)/by'")
+    }
+    if (n < 0L) 
+      stop("wrong sign in 'by' argument")
+    if (n > .Machine$integer.max) 
+      stop("'by' argument is much too small")
+    if (finite.del && abs(del)/max(abs(to), abs(from)) < 
+        100 * .Machine$double.eps) 
+      return(from)
+    if (int) {
+      n <- as.integer(n)
+      if (n >= 2L) 
+        cumsum(rep.int(c(from, by), c(1L, n)))
+      else from + (0L:n) * by
+    }
+    else {
+      n <- as.integer(n + 1e-10)
+      x <- if (finite.del) 
+        from + (0L:n) * by
+      else (from/4 + (0L:n) * (by/4)) * 4
+      if (by > 0) 
+        pmin(x, to)
+      else pmax(x, to)
+    }
+  }
+  else if (!is.finite(length.out) || length.out < 0L) 
+    stop("'length.out' must be a non-negative number")
+  else if (length.out == 0L) 
+    integer()
+  else if (One) 
+    seq_len(length.out)
+  else if (missing(by)) {
+    if (missing(to)) {
+      to <- from + (length.out - 1)
+      intdel <- intn1 && is.logint(from) && to <= .Machine$integer.max
+      if (intdel) 
+        storage.mode(to) <- "integer"
+    }
+    else intdel <- is.logint(to)
+    if (missing(from)) {
+      from <- to - (length.out - 1)
+      if (intdel) {
+        intdel <- intn1 && from >= -.Machine$integer.max
+        if (intdel) 
+          storage.mode(from) <- "integer"
+      }
+    }
+    else if (intdel) 
+      intdel <- is.logint(from)
+    if (length.out > 2L) 
+      if (from == to) 
+        rep.int(from, length.out)
+    else {
+      n1 <- length.out - 1L
+      if (intdel && intn1 && from%%n1 == to%%n1) {
+        by <- to%/%n1 - from%/%n1
+        cumsum(rep.int(c(from, by), c(1L, n1)))
+      }
+      else {
+        if (intdel) 
+          storage.mode(from) <- "double"
+        del <- to - from
+        if (is.finite(del)) {
+          as.vector(c(from, from + seq_len(length.out - 
+                                             2L) * (del/n1), to))
+        }
+        else {
+          from <- from/4
+          to <- to/4
+          as.vector(c(from, from + seq_len(length.out - 
+                                             2L) * ((to - from)/n1), to)) * 4
+        }
+      }
+    }
+    else as.vector(c(from, to))[seq_len(length.out)]
+  }
+  else if (missing(to)) {
+    int <- (intby <- is.logint(by)) && is.logint(from) && 
+      (!(nby <- length(by)) || (naby <- is.na(by)) || 
+         ((to <- from + (length.out - 1) * by) <= .Machine$integer.max && 
+            to >= -.Machine$integer.max))
+    if (int && length.out > 2L && nby == 1L && !naby) 
+      cumsum(rep.int(c(from, by), c(1L, length.out - 1L)))
+    else {
+      if (intby && !(int || is.object(from))) 
+        storage.mode(by) <- "double"
+      from + (0L:(length.out - 1L)) * by
+    }
+  }
+  else if (missing(from)) {
+    int <- (intby <- is.logint(by)) && is.logint(to) && 
+      (!(nby <- length(by)) || (naby <- is.na(by)) || 
+         ((from <- to - (length.out - 1) * by) >= -.Machine$integer.max && 
+            from <= .Machine$integer.max))
+    if (int && length.out > 2L && nby == 1L && !naby) 
+      cumsum(rep.int(c(as.integer(from), by), c(1L, length.out - 
+                                                  1L)))
+    else {
+      if (intby && !(int || is.object(to))) 
+        storage.mode(by) <- "double"
+      to - ((length.out - 1L):0L) * by
+    }
+  }
+  else stop("too many arguments")
+}
+
+ln <- function(a) {
+   return(a^2+cos(a))
+}
+
+ve <- function(a) {
+  b = c(2,2);
+  if (a > -129 && a < 128) {
+    b = a + 128;
+    print(b)
+  }
+  return(a)
+}
+
+k8o <- function(a, b, c, d, e) {
+  f = c(a+b^c*d+e);
+  if (!f) {
+    print(f)
+  }
+  return(call(name = "raf"))
+}
+
+Me <- function(b, c) {
+  d = c(0,5); 
+  f = c(0,7); 
+  h = c(0,9);
+  if (!c) {
+      print(d+f^h)
+    }
+    ++b;
+    i = 9
+    while (i > i) {
+      g = i ? i : i;
+        a = KHD(a);
+        if (N(a, b = 101)) {
+          f = a;
+          !d && c(d);
+          print(d)
+        } else {
+          LHD(a)
+        }
+      }
+    --b;
+    b == 0 && c(b)
+}  
+
+Pte <- function(h, b.b, b.e) {
+     if (h != b.b) {
+         print(h^b.b)
+     } else {
+       return(h)
+     }
+  if (b.b != length(0L)) {
+      print(b.b+sin(h))
+  } else {
+    return(b.b)
+  }
+  if (b.e != is.array(b.e)) {
+     print(b.e^2+cos(h))
+  } else {
+    return(b.e)
+  }
+}
+jF <- function(b, h) {
+      if (b != h) {
+          print(b^h)
+      } else {
+        return(cosh(b))
+      }
+  if (h != b) {
+      print(h^sin(b))
+  } else {
+    return(cos(h))
+  }
+}
+bF <- function(h, b.d, d) {
+    if (h != b.d) {
+        print(h^b.d)
+    } else {
+      return(h)
+    }
+  if (b.d != d) {
+      print(b.d*sin(d))
+  } else {
+    return(b.d)
+  }
+  if (d != is.array(h)) {
+      print(d^h+sin(b.d)) 
+  } else {
+    return(d)
+  }
+}
+
+Qte <- function(h) {
+   return(h^9)
+}
+gF <- function(b, c, d) {
+  return(b+c^sin(d))
+}
+
+Mjs <- function (a) {
+  a.e = FALSE;
+  a.n = FALSE;
+  a.a = NULL;
+  return(FALSE)
+}
+
+vc <- function(a, b) {
+  return(wnd.setTimeout(d = a, c = b))
+}
+
+a.mm <- function() {
+  returnValue(default = NULL)
+}
+mc <- function(a, b) {
+  if (b < a || length(0L)) {
+    LHD(a = b);
+  }
+  a = a.mm();
+  a.j = FALSE;
+  a.k = c(a, a.j)
+  return(a+b)
+}
+
+mEe <- function(a.e, b) {
+  if (a.e != b) {
+    a.e <- sin(b)
+    print(a.e)
+  } else {
+    return(a.e, b)
+  }
+  if (b != length(0L)) {
+    print(b)
+  } else {
+    return(b)
+  }
+}
+
+qye <- function(a, b) {
+  return(a+b+sin(a)+cos(b))
+}
+
+Lj <- function(a = -1) {
+  print(a^sin(a))
+}
+
+Na <- function(a = -1) {
+  if (a != -1) {
+     Lj <- function(a = -1) {
+         print(a^sin(a))
+     }
+     Lj(a = -1);
+  } else {
+    return(a)
+  }
+  if (0 < -1) {
+      Lj(a = -1);
+  } else {
+    return(length(0L))
+  }
+  if (0 == -1) {
+      Lj(a = -1);
+  } else {
+    return(-1)
+  }
+}
+# portability am push
+am = seq(a+A)
+
+# push k partial am
+K = Na(a = am)
+# type push link select
+Na(a = am) < K
+# search m
+m = 25
+# intelligence m
+Pm = Lj(a = am)
+# static Rn
+Rn = Pm || 0 < k
+# conversion vectors
+E > 0
+# static m about vector me
+me = 125
+m = 12
+m > me
+# static partial vector Lj
+Pj = 2510
+Lj(a = -1) < Pj + E + for(j in Pj) {
+  print(j^Pj+sin(a))
+}
+p = 25
+# split vector Na - 1
+Na(a = -1);Pj = Lj(a = -1) || c(Pj + E) || 0 < Pj + E || c(1, 1) || 0 
+Na(a = -1) < K || p || 0 < K ^ E
+# split vector to reference static d and b
+d <- function(ab) {
+  if (ab != length(0L)) {
+      ab <- p.adjust(ab, method = p.adjust.methods, n = length(0L))
+      print(ab^4)
+  } else {
+    return(ab)
+  }
+  if(a != -1) {
+     print(a^4)
+  } else {
+    return(a)
+  }
+}
+# fraction of vector to linear equation
+fr = 615
+w = 24
+b = 2
+# equation linear of fraction less what
+awb <- fr < a || w || b
+# tools static develop form
+a;b = A
+w = fr
+# following portability
+cp2 <- c(a, d(ab = 12), fr)
+# check flip calculation 
+La(a = -1);awb = d(ab = 12) || fr
+# strongly L and A
+L = A
+L1 <- function(f, mustFind = FALSE, where, package = "") 
+{
+  if (is.function(f)) {
+    if (is(f, "genericFunction")) 
+      return(f)
+    else if (is.primitive(f)) 
+      return(genericForBasic(.primname(f), mustFind = mustFind))
+    else stop("argument 'f' must be a string, generic function, or primitive: got an ordinary function")
+  }
+  value <- if (missing(where)) 
+  if (is.null(value) && !is.null(baseDef <- baseenv()[[f]])) {
+    if (is.function(baseDef)) {
+      value <- genericForBasic(f, mustFind = FALSE)
+      if (is(value, "genericFunction")) 
+        value <- .cacheGeneric(f, value)
+    }
+  }
+  if (is.function(value)) 
+    value
+  else {
+    if (nzchar(package) && is.na(match(package, c("methods", 
+                                                  "base")))) {
+      value <- tryCatch({
+        ev <- getNamespace(package)
+      }, error = function(e) NULL)
+    }
+    if (is.function(value)) 
+      value
+    else if (mustFind) 
+      stop(gettextf("no generic function found for %s", 
+                    sQuote(f)), domain = NA)
+    else NULL
+  }
+}
+# push location to type static values packages handle following layout
+d(ab = 12);L = d(ab = -1);d(ab = 1-1);+a^d(ab = -1) 
+# thus select form pass to types expression values select handle type link ...
+d(ab = 15);L = -a^-1;d(ab = a^-1)
+# normality personal static values handle type form using tables
+La(a = -1) < a^-1 || La(a = -1)
+# Leibniz values strongly handle using tables managers
+X = 0
+s1 = c(X, p)
+# expression values strongly tables managers reference link go link
+x0 = 50
+x1 = 51
+# values of link handle packages and managers
+if(x0 != x1) {
+  fr <- c(x0,x1)
+  A = c(X)
+  print(fr*pi)
+} else {
+   fw <- function() {
+     f <- function(fx0) {
+        print(fx0^w)
+     }
+     f(fx0 = 650)
+   }
+   fw()
+   wf <- function(x1) {
+      print(x1^27)
+   }
+   wf(x1 = 3)
+}
+
+# define d by ...
+f <- function(fx0) {
+  print(fx0^w)
+}
+df <- function(x1, x0) {
+  # static values factor handle logic util method reference topic
+   s1 <- f(fx0 = x1)
+   s2 <- f(fx0 = x0)
+   # token pass support link tile topic reference packages
+   p <- function(x1, x0) {
+     x1 <- seq(x1)
+     x0 <- seq(x0)
+     print(x1, x0)
+   }
+   s1/p(x1= x1, x0 = x0)
+   s2/p(x1= x1, x0 = x0)
+}
+
+
